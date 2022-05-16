34a7b447a921e7d5e1fbda7862e1eee2c79d91ce
faisalk2 <faisal.z.khan5538@gmail.com>
2022-05-16 23:26:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Mobile1,{Mobile2,Mobile3,Mobile4} from "./components/mobile"
+
+function App() {
+  return (
+    <div className="App">
+      <Mobile1></Mobile1>
+      <Mobile2></Mobile2>
+      <Mobile3></Mobile3>
+      <Mobile4></Mobile4>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mobile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Mobile1 = () => {
+    return <h1>Mobile Operating System</h1>
+}
+const Mobile2 = () => {
+    return <ul>
+        <li>Android</li>
+        <li>BlackBerry</li>
+        <li>Iphone</li>
+        <li>Windows Phone</li>
+    </ul>
+}
+const Mobile3 = () => {
+    return <h1>Mobile Manufacturers</h1>
+}
+const Mobile4 = () => {
+    return <ul>
+        <li>Samsunf</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Aphone</li>
+    </ul>
+}
+export default Mobile1;
+export { Mobile2, Mobile3, Mobile4 };
